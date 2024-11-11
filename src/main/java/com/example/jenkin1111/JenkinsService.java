0572f1391f4fe4ae9ee993dde3490dcb364cfa7e
itstudy <itstudy@kakao.com>
2024-11-11 14:27:02 +0900

    webhook

--- src/main/java/com/example/jenkin1111/JenkinsService.java
+++ src/main/java/com/example/jenkin1111/JenkinsService.java
@@ -3,8 +3,8 @@ package com.example.jenkin1111;
 public class JenkinsService {
 
     public int  hap(int n){
-        int result = 0;
-        for(int i=1; i<=n; i++){
+        int result = 1;
+        for(int i=2; i<=n; i++){
             result += i;
         }
         return result;
